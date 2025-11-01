d7ddb8d072480f53be9053c94f12d5ddd448a713
Yashuchirag <chiragchandrashekar@gmail.com>
2025-11-01 12:37:12 -0700

    Updated 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import SearchPage from './pages/SearchPage';
 import SearchHistoryPage from './pages/SearchHistoryPage';
+import NotFoundPage from './pages/404';
 import Footer from './components/Footer';
 import { Toaster } from 'react-hot-toast';
 import { useAuthUser } from './store/authUser';
@@ -40,6 +41,7 @@ function App() {
         <Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to="/login" />} />
         <Route path="/search" element={user ? <SearchPage /> : <Navigate to="/login" />} />
         <Route path="/history" element={user ? <SearchHistoryPage /> : <Navigate to="/login" />} />
+        <Route path='/*' element={<NotFoundPage />} />
       </Routes>
       <Footer />
       
